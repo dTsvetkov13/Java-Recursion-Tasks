0240e8c0564593fbfa2b46234b3d5db66f4db22e
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-02-14 20:12:33 +0200

    Forth solution added

---
+++ src/solutions/Forth.java
@@ -0,0 +1,42 @@
+package solutions;
+
+import java.util.Scanner;
+
+public class Forth
+{
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter n: ");
+		int n = scan.nextInt();
+		
+		printNumberFromRight(n);
+		System.out.println(n);
+		
+		scan.close();
+	}
+	
+	public static void printNumberFromRight(int number)
+	{
+		String numberAsString = Integer.toString(number);
+		
+		printNumberFromRight(numberAsString, numberAsString.length() - 1);
+	}
+	
+	public static void printNumberFromRight(String number, int index)
+	{
+		if(index == 0)
+		{
+			if(number.charAt(index) != '-')
+			{
+				System.out.print(number.charAt(index));
+			}
+			System.out.println();
+			return;
+		}
+		
+		System.out.print(number.charAt(index) + ", ");
+		printNumberFromRight(number, index - 1);
+	}
+}
