520f4a13c737b0257b93628a121c541c7b09dd3e
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-02-14 18:27:16 +0200

    First task

---
+++ src/solutions/First.java
@@ -0,0 +1,26 @@
+package solutions;
+
+import java.util.Scanner;
+
+public class First
+{
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter n: ");
+		int n = scan.nextInt();
+		
+		System.out.println("Sum: " + sumInRange(0, n));
+	}
+	
+	public static int sumInRange(int currentNum, int lastNum)
+	{
+		if(currentNum == lastNum)
+		{
+			return lastNum;
+		}
+		
+		return currentNum + sumInRange(currentNum + 1, lastNum);
+	}
+}
